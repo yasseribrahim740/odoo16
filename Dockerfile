68e481a07dbc2c6a3f96139c2a9ad199da722f3c
yasseribrahim740 <yasseribrahim7400@gmail.com>
2023-12-18 01:04:02 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM odoo:15
+
+# execute as root
+USER root
+
+# install postgresql-client-15
+RUN echo 'deb http://apt.postgresql.org/pub/repos/apt/ bullseye-pgdg main' > /etc/apt/sources.list.d/pgdg.list \
+    && GNUPGHOME="$(mktemp -d)" \
+    && export GNUPGHOME \
+    && repokey='B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8' \
+    && gpg --batch --keyserver keyserver.ubuntu.com --recv-keys "${repokey}" \
+    && gpg --batch --armor --export "${repokey}" > /etc/apt/trusted.gpg.d/pgdg.gpg.asc \
+    && gpgconf --kill all \
+    && rm -rf "$GNUPGHOME" \
+    && apt-get update  \
+    && apt-get install --no-install-recommends -y postgresql-client-15 \
+    && rm -f /etc/apt/sources.list.d/pgdg.list \
+    && rm -rf /var/lib/apt/lists/*
+
+# remove postgresql-client-16
+RUN apt remove postgresql-client-16 -y
---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+DESTINATION=$1
+PORT=$2
+CHAT=$3
+# clone Odoo directory
+git clone --depth=1 https://github.com/yasseribrahim740/odoo16 $DESTINATION
+rm -rf $DESTINATION/.git
+# set permission
+mkdir -p $DESTINATION/postgresql
+mkdir -p $DESTINATION/enterprise
+mkdir -p $DESTINATION/workspace
+cp -r ./enterprise/* $DESTINATION/enterprise
+cp -r ./workspace/* $DESTINATION/workspace
+sudo chmod -R 777 $DESTINATION
+# config
+if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
+sudo sysctl -p
+sed -i 's/10016/'$PORT'/g' $DESTINATION/docker-compose.yml
+sed -i 's/20016/'$CHAT'/g' $DESTINATION/docker-compose.yml
+# run Odoo
+docker-compose -f $DESTINATION/docker-compose.yml up -d
+
+echo 'Started Odoo @ http://localhost:'$PORT' | Master Password: yasseribrahim740 | Live chat port: '$CHAT
