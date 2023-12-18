6037eab3793e9181d8f12cddbc548d4afca99b7f
yasseribrahim740 <yasseribrahim7400@gmail.com>
2023-12-18 22:56:50 +0200

    edit conf

--- run.sh
+++ run.sh
@@ -7,10 +7,7 @@ git clone --depth=1 https://github.com/yasseribrahim740/odoo16 $DESTINATION
 rm -rf $DESTINATION/.git
 # set permission
 mkdir -p $DESTINATION/postgresql
-mkdir -p $DESTINATION/enterprise
-mkdir -p $DESTINATION/workspace
-cp -r ./enterprise/* $DESTINATION/enterprise
-cp -r ./workspace/* $DESTINATION/workspace
+cp -r ./addons/* $DESTINATION/addons
 sudo chmod -R 777 $DESTINATION
 # config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
